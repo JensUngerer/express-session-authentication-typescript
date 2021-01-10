733dea24d12a767d744dc1064a9b2d2fda3bac9e
Jens Ungerer <github@jens-ungerer.de>
2021-01-10 08:55:21 +0100

    adding dependencies and importing them

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
-import { App } from './app/app';
-import { Logger } from './app/common/logger';
+import express, { Request, Response} from 'express';
+import mongoose from 'mongoose';
+import session from 'express-session';
+import passport from 'passport';
+import crypto from 'crypto';
+import passportLocal from 'passport-local';
+const LocalStrategy = passportLocal.Strategy;
+import connectMongo, { MongooseConnectionOptions, MongoUrlOptions, NativeMongoOptions, NativeMongoPromiseOptions } from 'connect-mongo';
+const MongoStore = connectMongo(session);
+import dotenv from 'dotenv';
 
-Logger.logTask('SYSTEM', 'STARTING');
+// ?
+// const { ensureAuthenticated } = require('connect-ensure-authenticated');
+// const { ensureScope } = require('connect-ensure-authorization');
 
-App.run();
-
-Logger.logTask('SYSTEM', 'FINISHED');
